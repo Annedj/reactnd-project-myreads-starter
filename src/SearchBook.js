81bdebd11a773537ac278c175313cab8126f1a38
Anne de Joly <anne.dejoly@edhec.com>
2020-04-07 15:41:06 +0100

    added debounce to search input

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
-import Book from './Book'
+import Book from './Book';
 import PropTypes from 'prop-types';
+import debounce from 'lodash.debounce'
 
 class SearchBook extends Component {
   state = {
     query: '',
     searchBooks: []
-  };
+  }
 
   updateQuery = (query) => {
-    this.setState({query})
-
-    if (!query) { this.setState({
-        searchBooks: []
-      })
-    } else {
-      this.searchForBooks(query)
-    }
+    this.setState({query, searchBooks: []})
+    console.log('query is: ', this.state.query)
+      if (query === '') { this.setState({
+          searchBooks: []
+        })
+      } else {
+        this.searchForBooks(query)
+      }
   };
 
-  searchForBooks = (query) => {
+  searchForBooks = debounce((query) => {
+    console.log('the api will be called')
     BooksAPI.search(query)
       .then((searchBooks) => {
-        const updatedListBooks = this.updateWithExistingBooks(searchBooks)
+        console.log('searchbooks is: ', searchBooks)
+        const updatedListBooks = searchBooks ? this.updateWithExistingBooks(searchBooks) : []
         this.setState({
           searchBooks: updatedListBooks
         })
     })
-  };
+  }, 100);
 
   updateWithExistingBooks = (searchBooks) => {
-    let mergedArray = []
-    searchBooks.forEach((searchBook) => {
-      let bookToFind = this.props.books.find((existingBook) => existingBook.id === searchBook.id)
-      if (bookToFind) {
-        mergedArray.push(bookToFind)
-      } else {
-        mergedArray.push(searchBook)
-      }
-    })
+    const mergedArray = []
+    if (!searchBooks.error) {
+      searchBooks.forEach((searchBook) => {
+        let bookToFind = this.props.books.find((existingBook) => existingBook.id === searchBook.id)
+        if (bookToFind) {
+          mergedArray.push(bookToFind)
+        } else {
+          mergedArray.push(searchBook)
+        }
+      })
+    }
     return mergedArray
   }
 
   render() {
     const { query, searchBooks } = this.state
     const { moveShelf } = this.props
-    console.log('from render ', this.state)
 
     return (
       <div className="search-books">
@@ -66,7 +70,7 @@ class SearchBook extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {searchBooks.error && (<em><p>No result found for '{query}'. Please try another term.</p></em>)}
+            {(query && searchBooks.length === 0 ) && (<em><p>No result found for '{query}'. Please try another term.</p></em>)}
 
             {searchBooks.length > 0 && searchBooks.map((book) => (<Book key={book.id} book={book} moveShelf={moveShelf}/>))}
 
